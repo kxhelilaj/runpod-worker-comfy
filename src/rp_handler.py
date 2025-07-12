470487901888076ca8a81cbe45e6cebfb348dffd
kejvi <kejvixh12@gmail.com>
2025-07-12 22:45:30 +0200

    handle video outputs

--- src/rp_handler.py
+++ src/rp_handler.py
@@ -17,9 +17,11 @@ COMFY_POLLING_MAX_RETRIES = int(os.environ.get("COMFY_POLLING_MAX_RETRIES", 500)
 COMFY_HOST = "127.0.0.1:8188"
 REFRESH_WORKER = os.environ.get("REFRESH_WORKER", "false").lower() == "true"
 
+
 def validate_input(job_input):
     if job_input is None:
         return None, "Please provide input"
+
     if isinstance(job_input, str):
         try:
             job_input = json.loads(job_input)
@@ -37,6 +39,7 @@ def validate_input(job_input):
 
     return {"workflow": workflow, "images": images}, None
 
+
 def check_server(url, retries=500, delay=50):
     for _ in range(retries):
         try:
@@ -47,8 +50,9 @@ def check_server(url, retries=500, delay=50):
         except requests.RequestException:
             pass
         time.sleep(delay / 1000)
-    print(f"runpod-worker-comfy - Failed to connect to server at {url} after {retries} attempts.")
+    print(f"runpod-worker-comfy - Failed to connect to server at {url}")
     return False
+
 
 def upload_images(images):
     if not images:
@@ -58,6 +62,7 @@ def upload_images(images):
     upload_errors = []
 
     print(f"runpod-worker-comfy - image(s) upload")
+
     for image in images:
         name = image["name"]
         blob = base64.b64decode(image["image"])
@@ -72,46 +77,55 @@ def upload_images(images):
             responses.append(f"Successfully uploaded {name}")
 
     if upload_errors:
+        print(f"runpod-worker-comfy - image(s) upload with errors")
         return {"status": "error", "message": "Some images failed to upload", "details": upload_errors}
 
     print(f"runpod-worker-comfy - image(s) upload complete")
     return {"status": "success", "message": "All images uploaded successfully", "details": responses}
+
 
 def queue_workflow(workflow):
     data = json.dumps({"prompt": workflow}).encode("utf-8")
     req = urllib.request.Request(f"http://{COMFY_HOST}/prompt", data=data)
     return json.loads(urllib.request.urlopen(req).read())
 
+
 def get_history(prompt_id):
     with urllib.request.urlopen(f"http://{COMFY_HOST}/history/{prompt_id}") as response:
         return json.loads(response.read())
 
-def base64_encode(img_path):
-    with open(img_path, "rb") as f:
+
+def base64_encode(file_path):
+    with open(file_path, "rb") as f:
         return base64.b64encode(f.read()).decode("utf-8")
 
-def process_output_images(outputs, job_id):
+
+def process_output_files(outputs, job_id):
     COMFY_OUTPUT_PATH = os.environ.get("COMFY_OUTPUT_PATH", "/comfyui/output")
+    output_file_path = None
 
     for node_id, node_output in outputs.items():
         if "images" in node_output:
             for image in node_output["images"]:
-                local_path = os.path.join(COMFY_OUTPUT_PATH, image["subfolder"], image["filename"])
-                print(f"runpod-worker-comfy - checking for image at {local_path}")
-                if os.path.exists(local_path):
-                    if os.environ.get("BUCKET_ENDPOINT_URL"):
-                        uploaded_url = rp_upload.upload_image(job_id, local_path)
-                        return {"status": "success", "message": uploaded_url}
-                    else:
-                        b64_image = base64_encode(local_path)
-                        return {"status": "success", "message": b64_image}
-                else:
-                    print(f"runpod-worker-comfy - file does not exist at {local_path}")
+                output_file_path = os.path.join(image["subfolder"], image["filename"])
+        elif "videos" in node_output:
+            for video in node_output["videos"]:
+                output_file_path = os.path.join(video["subfolder"], video["filename"])
 
-    return {
-        "status": "error",
-        "message": "No images were found in the workflow outputs."
-    }
+    if not output_file_path:
+        return {"status": "error", "message": "No images or videos were found in the workflow outputs."}
+
+    local_file_path = os.path.join(COMFY_OUTPUT_PATH, output_file_path)
+
+    if os.path.exists(local_file_path):
+        if os.environ.get("BUCKET_ENDPOINT_URL"):
+            url_or_data = rp_upload.upload_image(job_id, local_file_path)
+        else:
+            url_or_data = base64_encode(local_file_path)
+        return {"status": "success", "message": url_or_data}
+    else:
+        return {"status": "error", "message": f"The file does not exist: {local_file_path}"}
+
 
 def handler(job):
     job_input = job["input"]
@@ -129,13 +143,13 @@ def handler(job):
         return upload_result
 
     try:
-        queued = queue_workflow(workflow)
-        prompt_id = queued["prompt_id"]
+        queued_workflow = queue_workflow(workflow)
+        prompt_id = queued_workflow["prompt_id"]
         print(f"runpod-worker-comfy - queued workflow with ID {prompt_id}")
     except Exception as e:
         return {"error": f"Error queuing workflow: {str(e)}"}
 
-    print(f"runpod-worker-comfy - waiting for image generation")
+    print(f"runpod-worker-comfy - wait until image generation is complete")
     retries = 0
     try:
         while retries < COMFY_POLLING_MAX_RETRIES:
@@ -149,7 +163,13 @@ def handler(job):
     except Exception as e:
         return {"error": f"Error waiting for image generation: {str(e)}"}
 
-    return {**process_output_images(history[prompt_id]["outputs"], job["id"]), "refresh_worker": REFRESH_WORKER}
+    output_result = process_output_files(history[prompt_id].get("outputs"), job["id"])
+
+    if output_result["status"] != "success":
+        raise ValueError(f"Job completed but failed: {output_result}")
+
+    return {**output_result, "refresh_worker": REFRESH_WORKER}
+
 
 if __name__ == "__main__":
     runpod.serverless.start({"handler": handler})
